0eb11bc799cc48d24b8e7ec456fffcfe3af4b347
sousagabriel14 <gabrielsousa140697@yahoo.com>
2020-06-23 14:26:00 -0300

    Adicionado Scroll animado para itens do menu

--- Course_Bootstrap/js/scripts.js
+++ Course_Bootstrap/js/scripts.js
@@ -154,4 +154,43 @@ $(document).ready(function() {
         }
     }
 
+        // Scroll para seções
+        let navBtn = $('.nav-item');  //'nav-item' são os itens do menu superior principal
+        
+        let bannerSection = $('#mainSlider');
+        let aboutSection = $('#about-area');
+        let servicesSection = $('#services-area');
+        let teamSection = $('#team-area');
+        let portfolioSection = $('#portfolio-area');
+        let contactSection = $('#contact-area');
+
+        let scrollTo = '';
+
+        //Criando evento de clique ao btn
+        $(navBtn).click(function() {
+
+            let btnId = $(this).attr('id');
+
+            // debug para saber se  esta passando os id's certos 'console.log(btnId);' 
+
+            // Condicionando os id's do menu para dar um scrollTo para a area desejada quando selecionado
+            if (btnId == 'about-menu') {
+                scrollTo = aboutSection;
+            } else if (btnId == 'services-menu') {
+                scrollTo = servicesSection;
+            } else if (btnId == 'team-menu') {
+                scrollTo = teamSection;
+            } else if (btnId == 'portfolio-menu') {
+                scrollTo = portfolioSection;
+            } else if (btnId == 'contact-menu'){
+                scrollTo = contactSection;
+            } else {
+                scrollTo = bannerSection;
+            }
+
+            //Criando a animação do scroll atrelado ao evento de click
+            $([document.documentElement, document.body]).animate({
+                scrollTop: $(scrollTo).offset().top - 70
+            }, 1500);
+        });
 });
